1f105ba7e4fa79ba4ae271c956b036bc9c115161
KinjalShah10 <shahkinjal0813@gmail.com>
2024-06-03 20:50:23 +0530

    Basics added

---
+++ Basics/Main.java
@@ -0,0 +1,9 @@
+package Basics;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}
