bd7d04f25b2625119a37d270218d50e5bb36d98e
KinjalShah10 <shahkinjal0813@gmail.com>
2024-06-11 23:22:47 +0530

    armstrong added

--- Functions and Methods/Armstrong.java
+++ Functions and Methods/Armstrong.java
@@ -1,3 +1,5 @@
+
+import java.util.Scanner;
 public class Armstrong {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -25,3 +27,4 @@ public class Armstrong {
 
         return sum == original;
     }
+}
