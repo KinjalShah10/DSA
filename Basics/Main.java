d679d63894bff6090bd5342a02938a76ee2e3f0f
KinjalShah10 <shahkinjal0813@gmail.com>
2024-06-11 23:22:37 +0530

    main added

--- Basics/Main.java
+++ Basics/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your name");
         System.out.println(input.nextLine());
     }
 }
